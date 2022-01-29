2766c85539aee958c482876fa13dac19d178e917
degrootdev <daan.groot2@gmail.com>
2022-01-29 17:03:49 +0100

    textmeshpro
    added textmeshpro and first highscore

---
+++ GlobalGameJam22/Scripts/HighScore.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HighScore : MonoBehaviour
+{
+    public TextMeshProUGUI scoreDisplay;
+    public float scoreMultiplayer = 1;
+    private float score;
+    private float time;
+    public LeaderBoard leaderbord;
+
+    //for testing
+    public bool dead = false;
+
+    private void Update()
+    {
+        if (!dead)
+        {
+            time += Time.deltaTime;
+            score = Mathf.Ceil(time) * scoreMultiplayer;
+            scoreDisplay.text = "Score: " + score;
+        }
+
+// for testting
+        if (dead)
+            leaderbord.SetNewScore(score);
+    
+    }
+}
---
+++ GlobalGameJam22/Scripts/LeaderBoard.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LeaderBoard : MonoBehaviour
+{
+  
+    public List<TextMeshProUGUI> leaderboardSpots = new List<TextMeshProUGUI>();
+    public List<float> scores = new List<float>();
+
+    /// <summary>
+    /// This function sets a new highscore on the leaderbord.
+    /// Je moeke can later be changed by name if we want player to use their own name.
+    /// </summary>
+    /// <param name="score"></param>
+    public void SetNewScore(float score)
+    {
+        if (score < scores.Count - 1)
+            return;
+
+        for (int i = 0; i < scores.Count; i++)
+        {
+            if (score > scores[i])
+            {
+                scores[i] = score;
+                leaderboardSpots[i].text = (i + 1) + "." + " je moeke " + scores[i];
+                break;
+            }
+        }
+    }
+}
