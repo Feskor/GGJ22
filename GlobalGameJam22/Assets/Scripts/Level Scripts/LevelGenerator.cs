bf5abf8ac4aba2d3d849748ae192b2f8a8ff66a3
KGroot <kwgroot@gmail.com>
2022-01-29 17:35:37 +0100

    Platforms now properly connect when size has changed EXCEPT when a turn is happening. When a turn is happening and size has changed the turn is cancelled and size is applied.

--- GlobalGameJam22/Assets/Scripts/Level Scripts/LevelGenerator.cs
+++ GlobalGameJam22/Assets/Scripts/Level Scripts/LevelGenerator.cs
@@ -7,20 +7,11 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField]
     [Range(10f, 50f)]
     private float platformSize;
-    public float PlatformSize
-    {
-        get { return platformSize; }
-        set 
-        { 
-            // Do some kind of check to make sure that the change in size still let's the player continue
-            // Connection check OR distance check with old and new platform size
-            platformSize = value;
-        }
-    }
+    private float prevPlatformSize, platformCorrection;
 
+    private float maxGameSpeed = 10;
     [Range(1f, 10f)]
-    public int gameSpeed;
-    private int baseGameSpeed = 10;
+    public float gameSpeed;    
 
     public GameObject level;
     private Queue<GameObject> platformsToRemove = new Queue<GameObject>();
@@ -30,17 +21,21 @@ public class LevelGenerator : MonoBehaviour
 
     private const int STARTPLATFORMS = 10;
     private int platformUntilTurnCount = 0;
-    private bool zDirection = true;
+    private bool zDirection = true, platformSizeChanged = false, goingNegative = false;
 
     private void Awake()
     {
+        prevPlatformSize = platformSize;
+
         // spawn STARTPLATFORMS amount of platforms before the game starts
         for (int i = 0; i < STARTPLATFORMS; i++)
-        {            
+        {
+            CreateMesh(currentPlatformCoord);
+
             if (i == 0)
-                CreateMesh(currentPlatformCoord, true);
+                GetNewCoords(currentPlatformCoord, true);
             else
-                CreateMesh(currentPlatformCoord);
+                GetNewCoords(currentPlatformCoord);
         }
 
         // When done start Coroutine
@@ -52,7 +47,9 @@ public class LevelGenerator : MonoBehaviour
     {
         CreateMesh(currentPlatformCoord);
 
-        yield return new WaitForSeconds(baseGameSpeed - gameSpeed);
+        yield return new WaitForSeconds(maxGameSpeed - gameSpeed);
+
+        GetNewCoords(currentPlatformCoord);
 
         StartCoroutine(GenerateLevel());
     }
@@ -94,13 +91,39 @@ public class LevelGenerator : MonoBehaviour
         mesh.triangles = triangles;
 
         GameObject platform = new GameObject("Platform " + middlePoint, typeof(MeshFilter), typeof(MeshRenderer));
+
         platform.transform.position = Vector3.zero;
+        if (zDirection && goingNegative) // moving z-axis to negative
+            platform.transform.position = new Vector3(platform.transform.position.x, platform.transform.position.y, platform.transform.position.z + platformCorrection);
+        else if (zDirection && !goingNegative)
+            platform.transform.position = new Vector3(platform.transform.position.x, platform.transform.position.y, platform.transform.position.z - platformCorrection);
+        else if (!zDirection && goingNegative)
+            platform.transform.position = new Vector3(platform.transform.position.x + platformCorrection, platform.transform.position.y, platform.transform.position.z);
+        else if (!zDirection && !goingNegative)
+            platform.transform.position = new Vector3(platform.transform.position.x - platformCorrection, platform.transform.position.y, platform.transform.position.z);
+
         platform.transform.localScale = Vector3.one;
         platform.GetComponent<MeshFilter>().mesh = mesh;
         platform.GetComponent<MeshRenderer>().material = platformMat;
         platform.AddComponent<BoxCollider>();
         platform.transform.parent = level.transform;
-        platform.GetComponent<MeshFilter>().mesh.RecalculateNormals();
+        platform.GetComponent<MeshFilter>().mesh.RecalculateNormals();     
+
+        platformsToRemove.Enqueue(platform);
+    }
+
+    public void GetNewCoords(Vector3 middlePoint, bool firstPlatform = false)
+    {
+        if (platformSize != prevPlatformSize)
+        {
+            platformCorrection = (platformSize - 10f) / 2f;
+
+            prevPlatformSize = platformSize;
+            platformSizeChanged = true;
+
+            // any value different than the base 10 needs a change
+            // (new number - 10 (base number)) / 2 = -z value
+        }            
 
         if (platformUntilTurnCount < STARTPLATFORMS)
             platformUntilTurnCount++;
@@ -108,17 +131,19 @@ public class LevelGenerator : MonoBehaviour
         if (platformUntilTurnCount >= STARTPLATFORMS)
         {
             // We can turn now
-            if (Random.Range(1, 11) == 1) // 1 in 10 chance to turn
+            if (Random.Range(1, 11) == 1 && !platformSizeChanged) // 1 in 10 chance to turn
             {
                 if (zDirection) // Swapping to X direction
                 {
                     if (Random.Range(1, 3) == 1) // Left turn (negative)
                     {
                         nextPlatformCoord = new Vector3(middlePoint.x - platformSize, middlePoint.y, middlePoint.z);
+                        goingNegative = true;
                     }
                     else // Right turn (positive)
                     {
                         nextPlatformCoord = new Vector3(middlePoint.x + platformSize, middlePoint.y, middlePoint.z);
+                        goingNegative = false;
                     }
                     zDirection = false;
                 }
@@ -127,17 +152,32 @@ public class LevelGenerator : MonoBehaviour
                     if (Random.Range(1, 3) == 1) // Left turn (negative)
                     {
                         nextPlatformCoord = new Vector3(middlePoint.x, middlePoint.y, middlePoint.z - platformSize);
+                        goingNegative = true;
                     }
                     else // Right turn (positive)
                     {
                         nextPlatformCoord = new Vector3(middlePoint.x, middlePoint.y, middlePoint.z + platformSize);
+                        goingNegative = false;
                     }
                     zDirection = true;
                 }
                 platformUntilTurnCount = 0;
             }
+            else if (platformSizeChanged)
+            {
+                if (zDirection && goingNegative) // moving z-axis to negative
+                    nextPlatformCoord = new Vector3(middlePoint.x, middlePoint.y, middlePoint.z - platformSize);
+                else if (zDirection && !goingNegative)
+                    nextPlatformCoord = new Vector3(middlePoint.x, middlePoint.y, middlePoint.z + platformSize);
+                else if (!zDirection && goingNegative)
+                    nextPlatformCoord = new Vector3(middlePoint.x - platformSize, middlePoint.y, middlePoint.z);
+                else if (!zDirection && !goingNegative)
+                    nextPlatformCoord = new Vector3(middlePoint.x + platformSize, middlePoint.y, middlePoint.z);
+
+                platformSizeChanged = false;
+            }
             else
-                nextPlatformCoord = (currentPlatformCoord + (currentPlatformCoord - prevPlatformCoord));
+                nextPlatformCoord = currentPlatformCoord + (currentPlatformCoord - prevPlatformCoord);
         }
         else // dont turn yet
         {
@@ -147,19 +187,30 @@ public class LevelGenerator : MonoBehaviour
                 currentPlatformCoord = Vector3.zero;
                 nextPlatformCoord = new Vector3(middlePoint.x, middlePoint.y, middlePoint.z + platformSize);
             }
+            else if (platformSizeChanged)
+            {
+                if (zDirection && goingNegative) // moving z-axis to negative
+                    nextPlatformCoord = new Vector3(middlePoint.x, middlePoint.y, middlePoint.z - platformSize);
+                else if (zDirection && !goingNegative)
+                    nextPlatformCoord = new Vector3(middlePoint.x, middlePoint.y, middlePoint.z + platformSize);
+                else if (!zDirection && goingNegative)
+                    nextPlatformCoord = new Vector3(middlePoint.x - platformSize, middlePoint.y, middlePoint.z);
+                else if (!zDirection && !goingNegative)
+                    nextPlatformCoord = new Vector3(middlePoint.x + platformSize, middlePoint.y, middlePoint.z);
+
+                platformSizeChanged = false;
+            }
             else
-                nextPlatformCoord = (currentPlatformCoord + (currentPlatformCoord - prevPlatformCoord));
+                nextPlatformCoord = currentPlatformCoord + (currentPlatformCoord - prevPlatformCoord);
         }
 
         prevPlatformCoord = currentPlatformCoord;
         currentPlatformCoord = nextPlatformCoord;
-
-        platformsToRemove.Enqueue(platform);
     }
 
     private IEnumerator RemovePlatform()
     {
-        yield return new WaitForSeconds(baseGameSpeed - gameSpeed);
+        yield return new WaitForSeconds(maxGameSpeed - gameSpeed);
 
         Destroy(platformsToRemove.Dequeue());
 
