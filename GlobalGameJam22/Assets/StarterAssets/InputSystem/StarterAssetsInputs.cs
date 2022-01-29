fcce5970163e85952852a992ddce6972a167f0a4
degrootdev <daan.groot2@gmail.com>
2022-01-29 17:00:00 +0100

    update player
    updated player script

--- GlobalGameJam22/Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ GlobalGameJam22/Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+using UnityEngine.InputSystem;
 #endif
 
 namespace StarterAssets
