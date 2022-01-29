18417b0cc266f24e98863ac02f035194a9f0008a
degrootdev <daan.groot2@gmail.com>
2022-01-29 22:28:37 +0100

    menu
    added menu

---
+++ GlobalGameJam22/Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OpenOptions()
+    {
+
+    }
+
+    public void CloseOptions()
+    {
+
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
